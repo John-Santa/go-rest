403fbfccefdb2285024ef0e756bc394f07cac069
John Santa <andersonsanta3@gmail.com>
2023-02-18 19:59:41 -0500

    Create routes

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/John-Santa/go-rest/routes"
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := mux.NewRouter()
+	router.HandleFunc("/", routes.HomeHandler)
+
+	http.ListenAndServe(":3000", router)
 }
